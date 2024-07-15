12268b0045038375d309df4ba98efb2d26ca62f5
deividsonsabino <deividsonsabino@outlook.com.br>
2024-07-15 19:21:32 -0300

    :sparkle: feat: create collision

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
 
